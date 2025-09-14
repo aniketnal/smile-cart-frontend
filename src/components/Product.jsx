072e764f8d92ddbd55b50e1aadd6bfbb5a297522
aniketnal <aniketnalegaonkar44@gmail.com>
2025-09-14 18:09:25 +0530

    added slides and dots navigation in carousel

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
-const Product = () => <div>Product Page</div>;
+import Carousel from "./Carousel";
+import { IMAGE_URLS } from "./constants";
+
+const Product = () => (
+  <div className="px-6 pb-6">
+    {/* rest of the code */}
+    <div className="w-2/5">
+      <Carousel imageUrls={IMAGE_URLS} title="Infinix Inbook" />
+    </div>
+    {/* rest of the code */}
+  </div>
+);
 
 export default Product;
