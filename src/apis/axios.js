b8335ae529451a0fc2d7869f9ab7fe11d35679a5
aniketnal <aniketnalegaonkar44@gmail.com>
2025-09-23 22:51:01 +0530

    Implemented search functionality to product listing page

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,9 +1,26 @@
 import axios from "axios";
-import { keysToCamelCase } from "neetocist";
+import { keysToCamelCase, serializeKeysToSnakeCase } from "neetocist";
+import { evolve } from "ramda";
 // ...
 
 const transformResponseKeysToCamelCase = response => {
   if (response.data) response.data = keysToCamelCase(response.data);
+};
+
+// request aane ke pehle edits
+const requestInterceptors = () => {
+  axios.interceptors.request.use(
+    evolve({ data: serializeKeysToSnakeCase, params: serializeKeysToSnakeCase })
+  );
+};
+
+// response dene se pehle edits
+const responseInterceptors = () => {
+  axios.interceptors.response.use(response => {
+    transformResponseKeysToCamelCase(response);
+
+    return response.data;
+  });
 };
 
 const setHttpHeaders = () => {
@@ -13,17 +30,10 @@ const setHttpHeaders = () => {
   };
 };
 
-const responseInterceptors = () => {
-  axios.interceptors.response.use(response => {
-    transformResponseKeysToCamelCase(response);
-
-    return response.data;
-  });
-};
-
 export default function initializeAxios() {
   axios.defaults.baseURL =
     "https://smile-cart-backend-staging.neetodeployapp.com/";
   setHttpHeaders();
   responseInterceptors();
+  requestInterceptors();
 }
